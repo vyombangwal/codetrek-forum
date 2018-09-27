fde10cd76e29e91fd23d112e78be588fecf3f490
unknown <vyombangwal@gmail.com>
2018-09-27 15:19:05 +0530

    th

---
+++ index.php
@@ -0,0 +1,112 @@
+<html>
+<head>
+	<title>forum</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <!--jQuery library--> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--Latest compiled and minified JavaScript--> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+
+	</head>
+	<body>
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="index.html" class="navbar-brand" style="color: white;">Code Trek Forum</a>
+                    <a href="index.html"  class="navbar-brand" style="font-size:1.2em;">Questions</a>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#my_navbar">
+                        <span class="icon-bar"></span>
+                          <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                    </button>
+               </div>
+                <div class="collapse navbar-collapse" id="my_navbar">
+                    <ul class="nav navbar-nav navbar-right">
+                       <li><span class="glyphicon glyphicon-cog navbar-brand" style=" "></span></li>
+                       <li><a href="#" class="navbar-brand" style=";"><span class="glyphicon glyphicon-triangle-bottom" ></span></a>
+  </li>
+                    </ul>
+                </div>
+               
+            </div>
+        </nav><br><br><br>
+<div class="container">
+	<div class="row">
+		<div class=" col-xs-1 " style="float:left;">
+			<h2 style="Times">Questions</h2>
+		</div>
+        <div class="col-xs-4	 col-xs-offset-5">
+    		
+            <div id="custom-search-input">
+                <div class="input-group col-xs-7 col-xs-offset-4">
+                    <input type="text" class="form-control input-md" style="" placeholder="Search your question" />
+                    <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" style="height:1.9em;" type="button">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-1" style=""><button class="btn btn-primary"  style="height:2.4em; background-color:white;color:black" type="button">Ask question</button></div>
+	</div>
+<br>
+<div class="card" style="width: 100%; border: 1px solid  grey ; border-radius:1em; ">
+  <div class="card-body" style="margin:2em;">
+    <h5 class="card-title" style=" font-size:2em; ">How do I use Git and GitHub?</h5>
+    <p class="card-text" >Quick sample text to create the card title and make up the body of the card's contentQuick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content..
+    <br>
+    <p><span style="color:blue;">Abhishek pokhriya</span> asked on Oct 2,2018</p>
+    </p>
+    <a href="#" class="" style="padding-left: 0.8em;" ><span class="glyphicon glyphicon-thumbs-up" style="color:black;">14</span></a>
+     <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-thumbs-down" style="color:black;">2</span></a>
+      <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-comment" style=>7</span></a>
+  </div>
+</div><br><br>
+<div class="card" style="width: 100%; border: 1px solid  grey ; border-radius:1em; ">
+  <div class="card-body" style="margin:2em;">
+    <h5 class="card-title" style=" font-size:2em; ">What will I learn from Code Trek?</h5>
+    <p class="card-text" >Quick sample text to create the card title and make up the body of the card's contentQuick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content..</p>
+    <br>
+    <p><span style="color:blue;">Abhishek pokhriya</span> asked on Oct 2,2018</p>
+    </p>
+    <a href="#" class="" style="padding-left: 0.8em;" ><span class="glyphicon glyphicon-thumbs-up" style="color:black;">14</span></a>
+     <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-thumbs-down" style="color:black;">2</span></a>
+      <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-comment" style=>7</span></a>
+  
+  </div>
+</div><br><br>
+<div class="card" style="width: 100%; border: 1px solid  grey ; border-radius:1em; ">
+  <div class="card-body" style="margin:2em;">
+    <h5 class="card-title" style=" font-size:2em; ">Where can I apply for Code Trek?</h5>
+    <p class="card-text" >Quick sample text to create the card title and make up the body of the card's contentQuick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content..</p>
+    <br>
+    <p><span style="color:blue;">Abhishek pokhriya</span> asked on Oct 2,2018</p>
+    </p>
+    <a href="#" class="" style="padding-left: 0.8em;" ><span class="glyphicon glyphicon-thumbs-up" style="color:black;">14</span></a>
+     <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-thumbs-down" style="color:black;">2</span></a>
+      <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-comment" style=>7</span></a>
+  
+  </div>
+</div><br><br>
+<div class="card" style="width: 100%; border: 1px solid  grey ; border-radius:1em; ">
+  <div class="card-body" style="margin:2em;">
+    <h5 class="card-title" style=" font-size:2em; ">Who is Vyom Bangwal?</h5>
+    <p class="card-text" >Quick sample text to create the card title and make up the body of the card's contentQuick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content.Quick sample text to create the card title and make up the body of the card's content..</p>
+    <br>
+    <p><span style="color:blue;">Abhishek pokhriya</span> asked on Oct 2,2018</p>
+    </p>
+    <a href="#" class="" style="padding-left: 0.8em;" ><span class="glyphicon glyphicon-thumbs-up" style="color:black;">14</span></a>
+     <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-thumbs-down" style="color:black;">2</span></a>
+      <a href="#" class="" style="padding-left: 0.8em;"><span class="glyphicon glyphicon-comment" style=>7</span></a>
+  
+  </div>
+</div>
+<br><br>
+
+
+</div>
+	</body>
+</html>
